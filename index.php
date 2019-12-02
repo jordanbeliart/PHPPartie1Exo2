13f46326f3aac36b34669b60fd5384eb9c248479
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-02 15:03:37 +0100

    exo2

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$lastname = 'DeMonMiraille';
+$firstname = 'Jean-Charles';
+$age = '39';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP Partie1 exo2</title>
+  </head>
+  <body>
+    <p>
+      <?php echo $lastname; ?>
+    </p>
+    <p>
+      <?php echo $firstname; ?>
+    </p>
+    <p>
+      <?php echo $age; ?>
+    </p>
+  </body>
+</html>
